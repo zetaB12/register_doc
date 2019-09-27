eb099bf9f3c9ac93dc3b741a8b3e476c92cbece7
Deyvi <deyvizavaleta@gmail.com>
2019-09-26 22:04:57 -0500

    system initial

---
+++ BusinessEntity/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity
+{
+    public class Class1
+    {
+        public string name { get; set; }
+
+    }
+}
---
+++ WebPresentation/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebPresentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var dey = new Class1();
+            ViewBag.nombre = dey.name = "deyvi";
+            return View();
+        }
+    }
+}
